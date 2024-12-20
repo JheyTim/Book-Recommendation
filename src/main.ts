bbd25b330fcc99d7c66cb7004e76cf4ca5d05e4c
Timothy Jhey Sarmiento <timothyjhey@gmail.com>
2024-12-21 03:32:58 +0800

    Added Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Book Recommendation API')
+    .setDescription(
+      'API for searching books, posting reviews, and getting recommendations',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
